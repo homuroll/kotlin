9cfec3f5adef9efcc6fa13f4f37ec2225a3d5515
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-02-03 14:03:30 +0300

    Introduce createCoroutineUnchecked intrinsic
    Also use it for buildSequence implenentation
    
     #KT-16155 Fixed

--- libraries/stdlib/common/src/kotlin/CoroutinesIntrinsicsH.kt
+++ libraries/stdlib/common/src/kotlin/CoroutinesIntrinsicsH.kt
@@ -19,27 +19,12 @@ package kotlin.coroutines.experimental.intrinsics
 import kotlin.coroutines.experimental.Continuation
 
 @SinceKotlin("1.1")
-@Suppress("UNCHECKED_CAST")
-@kotlin.internal.InlineOnly
-public inline fun <T> (suspend () -> T).startCoroutineUninterceptedOrReturn(
+public header fun <T> (suspend () -> T).createCoroutineUnchecked(
         completion: Continuation<T>
-): Any? = this.asDynamic()(completion, false)
+): Continuation<Unit>
 
 @SinceKotlin("1.1")
-@Suppress("UNCHECKED_CAST")
-@kotlin.internal.InlineOnly
-public inline fun <R, T> (suspend R.() -> T).startCoroutineUninterceptedOrReturn(
+public header fun <R, T> (suspend R.() -> T).createCoroutineUnchecked(
         receiver: R,
         completion: Continuation<T>
-): Any? = this.asDynamic()(receiver, completion, false)
-
-@SinceKotlin("1.1")
-public fun <R, T> (suspend R.() -> T).createCoroutineUnchecked(
-        receiver: R,
-        completion: Continuation<T>
-): Continuation<Unit> = this.asDynamic()(receiver, completion, true)
-
-@SinceKotlin("1.1")
-public fun <T> (suspend () -> T).createCoroutineUnchecked(
-        completion: Continuation<T>
-): Continuation<Unit> = this.asDynamic()(completion, true)
+): Continuation<Unit>
--- libraries/stdlib/src/kotlin/coroutines/experimental/intrinsics/IntrinsicsJvm.kt
+++ libraries/stdlib/src/kotlin/coroutines/experimental/intrinsics/IntrinsicsJvm.kt
@@ -17,7 +17,11 @@
 @file:kotlin.jvm.JvmName("IntrinsicsJvmKt")
 @file:kotlin.jvm.JvmVersion
 package kotlin.coroutines.experimental.intrinsics
-import kotlin.coroutines.experimental.*
+import kotlin.coroutines.experimental.Continuation
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.processBareContinuationResume
+import kotlin.coroutines.experimental.jvm.internal.CoroutineImpl
+import kotlin.coroutines.experimental.jvm.internal.interceptContinuationIfNeeded
 
 /**
  * Starts unintercepted coroutine without receiver and with result type [T] and executes it until its first suspension.
@@ -47,3 +51,49 @@ public inline fun <R, T> (suspend R.() -> T).startCoroutineUninterceptedOrReturn
         receiver: R,
         completion: Continuation<T>
 ): Any? = (this as Function2<R, Continuation<T>, Any?>).invoke(receiver, completion)
+
+@SinceKotlin("1.1")
+fun <T> (suspend () -> T).createCoroutineUnchecked(
+        completion: Continuation<T>
+): Continuation<Unit> =
+        if (this !is CoroutineImpl)
+            buildContinuationByInvokeCall(completion) {
+                (this as Function1<Continuation<T>, Any?>).invoke(completion)
+            }
+        else
+            (this.create(completion) as CoroutineImpl).facade
+
+@SinceKotlin("1.1")
+fun <R, T> (suspend R.() -> T).createCoroutineUnchecked(
+        receiver: R,
+        completion: Continuation<T>
+): Continuation<Unit> =
+        if (this !is CoroutineImpl)
+            buildContinuationByInvokeCall(completion) {
+                (this as Function2<R, Continuation<T>, Any?>).invoke(receiver, completion)
+            }
+        else
+            ((this as CoroutineImpl).create(receiver, completion) as CoroutineImpl).facade
+
+// INTERNAL DEFINITIONS
+
+private inline fun <T> buildContinuationByInvokeCall(
+        completion: Continuation<T>,
+        crossinline block: () -> Any?
+): Continuation<Unit> {
+    val continuation =
+            object : Continuation<Unit> {
+                override val context: CoroutineContext
+                    get() = completion.context
+
+                override fun resume(value: Unit) {
+                    processBareContinuationResume(completion, block)
+                }
+
+                override fun resumeWithException(exception: Throwable) {
+                    completion.resumeWithException(exception)
+                }
+            }
+
+    return completion.context.interceptContinuationIfNeeded(continuation)
+}
